1fa6853c80457a1053df9706751687b68e477c9e
Alexander Israel <adisrael@uc.cl>
2019-04-27 19:45:54 -0400

    api for clients and places

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,29 +4,54 @@ const pkg = require('../../package.json');
 const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
-  
-    ctx.body = { message: `Hello Agrobolt!` };
-
-  });
-
-router.get('clients', async (ctx) => {
-  
-  let clients = await ctx.orm.clients.findAll({
-    atributes: ["name"],
-  });
   ctx.body = {
-    clients
+    message: 'Hello Agrobolt!',
   };
 });
 
-router.get('places', async (ctx) => {
-
-  let clients = await ctx.orm.clients_places.findAll({
-    atributes: ["place_name"],
+router.get('clients', async (ctx) => {
+  const clients = await ctx.orm.clients.findAll({
+    atributes: ['name'],
   });
   ctx.body = {
-    clients
+    clients,
   };
+});
+
+router.get('clients.show', 'clients/:name', async (ctx) => {
+  const { name } = ctx.params;
+  const client = await ctx.orm.clients.findOne({
+    where: { name },
+  });
+  if (!client) {
+    ctx.body = `No se econtró el usuario ${name}`;
+    return ctx.body;
+  }
+  ctx.body = { client };
+  return ctx.body;
+});
+
+router.get('places', async (ctx) => {
+  const clientsPlaces = await ctx.orm.clients_places.findAll({
+    attributes: { exclude: ['polygon'] },
+  });
+  ctx.body = { clientsPlaces };
+  return ctx.body;
+});
+
+router.get('places.byClient', 'places/:clientName', async (ctx) => {
+  // entrega los campos del cliente client_name
+  const { clientName } = ctx.params;
+  const clientsPlaces = await ctx.orm.clients_places.findAll({
+    where: { client_name: clientName },
+    attributes: { exclude: ['polygon', 'client_name'] },
+  });
+  if (!clientsPlaces) {
+    ctx.body = `El cliente ${clientName} no existe o no tiene campos.`;
+    return ctx.body;
+  }
+  ctx.body = { clientsPlaces };
+  return ctx.body;
 });
 
 module.exports = router;
